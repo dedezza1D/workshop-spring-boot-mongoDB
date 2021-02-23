9b824c16008897a42c7f1d058467def1190215fa
Andrezza Medeiros <andrezzaammss@gmail.com>
2021-02-23 16:36:18 -0300

    End point to return a user's posts

--- src/main/java/com/andrezza/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andrezza/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andrezza.workshopmongo.domain.Post;
 import com.andrezza.workshopmongo.domain.User;
 import com.andrezza.workshopmongo.dto.UserDTO;
 import com.andrezza.workshopmongo.services.UserService;
@@ -44,20 +45,24 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
  	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
- 	public ResponseEntity<Void> Update(@RequestBody UserDTO objDto, @PathVariable String id) {
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
